147ccbc9250e2fef3e2853c41c4d4c07d6998b09
Marco D. Mosna <mmosna.cs@gmail.com>
2024-05-11 17:17:48 +0800

    chore: remove comments

--- src/main.rs
+++ src/main.rs
@@ -62,7 +62,7 @@ fn handle_client() {
     }
 }
 
-fn handle_server_connect() -> (Arc<TcpStream>, Arc<TcpStream>) {
+fn handle_server_connect() {
     // TODO: change this to be able to be defined by user
     let ip = local_ip();
     let addr = format!("{}:{}", ip.unwrap(), 6969);
@@ -71,38 +71,36 @@ fn handle_server_connect() -> (Arc<TcpStream>, Arc<TcpStream>) {
     let socket = Arc::new(socket);
 
     let r_socket = Arc::clone(&socket);
-    //std::thread::spawn(move || {
-    //    let mut r = BufReader::new(r_socket.as_ref());
-    //    loop {
-    //        let mut message = String::new();
-    //        if let Ok(bytes_read) = r.read_line(&mut message) {
-    //            if bytes_read == 0 {
-    //                println!("Server disconnected");
-    //                break;
-    //            }
-    //            println!("{}", message.trim());
-    //        }
-    //    }
-    //});
+    std::thread::spawn(move || {
+        let mut r = BufReader::new(r_socket.as_ref());
+        loop {
+            let mut message = String::new();
+            if let Ok(bytes_read) = r.read_line(&mut message) {
+                if bytes_read == 0 {
+                    println!("Server disconnected");
+                    break;
+                }
+                println!("{}", message.trim());
+            }
+        }
+    });
 
     let w_socket = Arc::clone(&socket);
-    //std::thread::spawn(move || {
-    //    let mut w = BufWriter::new(w_socket.as_ref());
+    std::thread::spawn(move || {
+        let mut w = BufWriter::new(w_socket.as_ref());
 
-    //    loop {
-    //        let mut input = String::new();
-    //        std::io::stdin()
-    //            .read_line(&mut input)
-    //            .expect("Failed to read from input");
+        loop {
+            let mut input = String::new();
+            std::io::stdin()
+                .read_line(&mut input)
+                .expect("Failed to read from input");
 
-    //        let message = format!("Hello from client: {}\n", input.trim());
-    //        w.write_all(message.as_bytes())
-    //            .expect("Failed to write to server");
-    //        w.flush().expect("Failed to flush buffer");
-    //    }
-    //});
-
-    (r_socket, w_socket)
+            let message = format!("Hello from client: {}\n", input.trim());
+            w.write_all(message.as_bytes())
+                .expect("Failed to write to server");
+            w.flush().expect("Failed to flush buffer");
+        }
+    });
 }
 
 fn main() {
@@ -113,52 +111,27 @@ fn main() {
         .read_line(&mut choice)
         .expect("Error reading line");
 
-    let (r_socket, w_socket): (Arc<TcpStream>, Arc<TcpStream>);
-
     match choice.trim() {
         "1" => {
             println!("Server stuff");
             handle_client();
-            (r_socket, w_socket) = todo!();
         }
         "2" => {
             println!("Client stuff");
-            (r_socket, w_socket) = handle_server_connect();
+            handle_server_connect();
         }
         _ => {
             println!("Invalid input, defaulting to server...");
             handle_client();
-            (r_socket, w_socket) = todo!();
         }
     }
 
-    let mut w = BufWriter::new(w_socket.as_ref());
-
     let (mut rl, thread) = raylib::init().size(640, 480).title("bangbang_six").build();
-
     rl.set_target_fps(60);
 
-    // TODO: Handle read
     while !rl.window_should_close() {
-        if rl.is_key_pressed(KeyboardKey::KEY_SPACE) {
-            w.write_all(b"Hello from client hihi\n")
-                .expect("failed to write message to server");
-            w.flush().expect("failed to flush buffer");
-        }
-
-        // TODO: make this non-blocking
-        //let mut r = BufReader::new(r_socket.as_ref());
-        //let mut message = String::new();
-        //if let Ok(bytes_read) = r.read_line(&mut message) {
-        //    if bytes_read == 0 {
-        //        println!("Server disconnected");
-        //        break;
-        //    }
-        //}
-
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::WHITE);
-        //d.draw_text(&message, 100, 10, 20, Color::BLACK);
         d.draw_fps(10, 10);
     }
 }
