2f1d05ad2a7952048ccbe80ba88ac9cc5f653c43
Marco D. Mosna <mmosna.cs@gmail.com>
2024-05-11 18:44:14 +0800

    feat: add client-server communcation

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,11 @@ use std::{
     sync::{mpsc, Arc},
 };
 
+use crossbeam::channel::{bounded, unbounded, Receiver};
 use local_ip_address::linux::local_ip;
 use raylib::prelude::*;
 
-fn handle_client(game_tx: mpsc::Sender<String>) {
+fn handle_client(game_tx: mpsc::Sender<String>, server_rx: Receiver<String>) {
     let ip = local_ip();
     let addr = format!("{}:{}", ip.unwrap(), 6969);
     let listener = TcpListener::bind(addr).expect("failed to bind to address");
@@ -18,17 +19,16 @@ fn handle_client(game_tx: mpsc::Sender<String>) {
                 let stream_arc = Arc::new(stream);
 
                 let w_stream = Arc::clone(&stream_arc);
+                let server_rx = server_rx.clone();
                 std::thread::spawn(move || {
                     let mut w = BufWriter::new(w_stream.as_ref());
 
                     // TODO: make this available in main thread
                     loop {
-                        let mut input = String::new();
-                        std::io::stdin()
-                            .read_line(&mut input)
-                            .expect("Failed to read from input");
-
-                        let message = format!("Hello from server: {}\n", input.trim());
+                        let mut message = String::new();
+                        if let Ok(recieved_msg) = server_rx.try_recv() {
+                            message = format!("Hello from server: {}\n", recieved_msg.trim());
+                        }
                         w.write_all(message.as_bytes())
                             .expect("Failed to write to client");
                         w.flush().expect("Failed to flush buffer");
@@ -64,7 +64,7 @@ fn handle_client(game_tx: mpsc::Sender<String>) {
     }
 }
 
-fn handle_server_connect(game_tx: mpsc::Sender<String>) {
+fn handle_server_connect(game_tx: mpsc::Sender<String>, server_rx: Receiver<String>) {
     // TODO: change this to be able to be defined by user
     let ip = local_ip();
     let addr = format!("{}:{}", ip.unwrap(), 6969);
@@ -92,12 +92,10 @@ fn handle_server_connect(game_tx: mpsc::Sender<String>) {
         let mut w = BufWriter::new(w_socket.as_ref());
 
         loop {
-            let mut input = String::new();
-            std::io::stdin()
-                .read_line(&mut input)
-                .expect("Failed to read from input");
-
-            let message = format!("Hello from client: {}\n", input.trim());
+            let mut message = String::new();
+            if let Ok(recieved_msg) = server_rx.try_recv() {
+                message = format!("Hello from server: {}\n", recieved_msg.trim());
+            }
             w.write_all(message.as_bytes())
                 .expect("Failed to write to server");
             w.flush().expect("Failed to flush buffer");
@@ -113,22 +111,23 @@ fn main() {
         .expect("Error reading line");
 
     let (game_tx, game_rx) = mpsc::channel();
+    let (server_tx, server_rx) = bounded(0);
 
     match choice.trim() {
         "1" => {
             println!("Server stuff");
             std::thread::spawn(move || {
-                handle_client(game_tx.clone());
+                handle_client(game_tx.clone(), server_rx.clone());
             });
         }
         "2" => {
             println!("Client stuff");
-            handle_server_connect(game_tx.clone());
+            handle_server_connect(game_tx.clone(), server_rx.clone());
         }
         _ => {
             println!("Invalid input, defaulting to server...");
             std::thread::spawn(move || {
-                handle_client(game_tx.clone());
+                handle_client(game_tx.clone(), server_rx.clone());
             });
         }
     }
@@ -138,7 +137,11 @@ fn main() {
 
     let mut message = String::new();
     while !rl.window_should_close() {
-        if rl.is_key_pressed(KeyboardKey::KEY_SPACE) {}
+        if rl.is_key_pressed(KeyboardKey::KEY_SPACE) {
+            let num: i32 = get_random_value(0, 6);
+            server_tx.send(format!("Rand num: {}", num)).unwrap();
+            println!("space presed");
+        }
 
         let mut d = rl.begin_drawing(&thread);
 
